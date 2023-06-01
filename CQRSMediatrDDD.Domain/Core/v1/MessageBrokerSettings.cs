8ba04ca348a50440922b76ddf53247bd9a567d37
Daniel Visicatto <dpfreestyle@gmail.com>
2023-06-01 13:52:48 -0300

    Implementados NotificationContext e settings do Broker

---
+++ CQRSMediatrDDD.Domain/Core/v1/MessageBrokerSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace CQRSMediatrDDD.Domain.Core.v1;
+
+public class MessageBrokerSettings
+{
+    public string? ConnectionString { get; set; }
+}
---
+++ CQRSMediatrDDD.Domain/Core/v1/NotificationContext.cs
@@ -0,0 +1,22 @@
+﻿using CQRSMediatrDDD.Domain.Contracts.v1;
+using CQRSMediatrDDD.Domain.ValueObjects.v1;
+
+namespace CQRSMediatrDDD.Domain.Core.v1;
+
+public class NotificationContext : INotificationContext
+{
+    private readonly List<Notification> _notifications;
+
+    public NotificationContext()
+    {
+        _notifications = new List<Notification>();
+    }
+
+    public IReadOnlyCollection<Notification> Notifications => _notifications;
+    public bool HasNotifications => _notifications.Any();
+
+    public void AddNotification(string notification)
+    {
+        _notifications.Add(new Notification(notification));
+    }
+}
