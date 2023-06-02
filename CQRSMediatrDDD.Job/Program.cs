8f2ffbe5edbf9527dc8682a8956b8ca8fbc61d3e
Daniel Visicatto <dpfreestyle@gmail.com>
2023-06-02 00:15:56 -0300

    Organizando pacotes Nuget, removendo classlibraries (conhecemos work)

---
+++ CQRSMediatrDDD.Job/Program.cs
@@ -0,0 +1,10 @@
+using CQRSMediatrDDD.Job;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
